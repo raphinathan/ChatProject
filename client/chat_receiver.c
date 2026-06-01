9f92138a53fb77adb07c829648e453f2eccef3ff
Raphi Nathan <rphnathan@gmail.com>
2026-06-01 19:15:42 +0300

    group name display

--- client/chat_receiver.c
+++ client/chat_receiver.c
@@ -1,7 +1,7 @@
 /* =========================================================================
  *  chat_receiver - standalone UDP multicast receiver.
  *
- *  Usage: ./chat_receiver <mcast_ip> <port>
+ *  Usage: ./chat_receiver <mcast_ip> <port> [msqid] [group_name]
  *
  *  Joins the given multicast group on the default interface and prints
  *  every datagram to stdout. Multiple receivers can share the same port
@@ -32,14 +32,16 @@ int main(int argc, char* argv[])
     const char*        mcast_ip;
     int                port;
     int                msqid = -1;
+    const char*        group_name = NULL;
     int                sockfd;
     int                reuse = 1;
     struct sockaddr_in bind_addr;
     struct ip_mreq     mreq;
     char               buf[MAX_DGRAM + 1];
 
-    if (argc != 3 && argc != 4) {
-        fprintf(stderr, "usage: %s <mcast_ip> <port> [msqid]\n", argv[0]);
+    if (argc < 3 || argc > 5) {
+        fprintf(stderr, "usage: %s <mcast_ip> <port> [msqid] [group_name]\n",
+                argv[0]);
         return 1;
     }
     mcast_ip = argv[1];
@@ -48,8 +50,11 @@ int main(int argc, char* argv[])
         fprintf(stderr, "bad port: %s\n", argv[2]);
         return 1;
     }
-    if (argc == 4) {
+    if (argc >= 4) {
         msqid = atoi(argv[3]);
+    }
+    if (argc == 5 && argv[4][0] != '\0') {
+        group_name = argv[4];
     }
 
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -67,9 +72,16 @@ int main(int argc, char* argv[])
     }
 
     memset(&bind_addr, 0, sizeof(bind_addr));
-    bind_addr.sin_family      = AF_INET;
-    bind_addr.sin_port        = htons((uint16_t)port);
-    bind_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    bind_addr.sin_family = AF_INET;
+    bind_addr.sin_port   = htons((uint16_t)port);
+    /* Bind to the group address (not INADDR_ANY) so the kernel filters by
+     * destination: with several groups sharing one port, each receiver only
+     * gets its own group's traffic instead of every group's (Linux). */
+    if (inet_pton(AF_INET, mcast_ip, &bind_addr.sin_addr) != 1) {
+        fprintf(stderr, "bad multicast ip: %s\n", mcast_ip);
+        close(sockfd);
+        return 1;
+    }
     if (bind(sockfd, (struct sockaddr*)&bind_addr, sizeof(bind_addr)) < 0) {
         perror("bind");
         close(sockfd);
@@ -102,8 +114,13 @@ int main(int argc, char* argv[])
         }
     }
 
-    fprintf(stderr, "[chat_receiver] joined %s:%d, waiting...\n",
-            mcast_ip, port);
+    if (group_name) {
+        fprintf(stderr, "[chat_receiver] group \"%s\" -- joined %s:%d, "
+                "waiting...\n", group_name, mcast_ip, port);
+    } else {
+        fprintf(stderr, "[chat_receiver] joined %s:%d, waiting...\n",
+                mcast_ip, port);
+    }
 
     for (;;) {
         ssize_t n = recvfrom(sockfd, buf, MAX_DGRAM, 0, NULL, NULL);
